9a9f923252a6dac5103708969e95e3a0a67ec8d5
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-12-17 17:36:31 -0600

    moving file structure for reading

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,26 @@
+const ProjectCard = props => {
+    return (
+        <div className="card sticky-action">
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={props.img} alt={props.title} />
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{props.title}</span>
+            </div>
+            <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">
+                    {props.title}
+                    <i className="material-icons right">close</i>
+                </span>
+                <br></br>
+                <p>{props.description}</p>
+            </div>
+            <div class="card-action">
+                <a href={props.site}><i className="material-icons left">link</i></a>
+                <a href={props.repo}><i className="material-icons right">code</i></a>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCard;
