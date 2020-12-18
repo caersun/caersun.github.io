8c3f9281f290aea08edcd6059eebf1bb52116656
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-12-17 18:01:00 -0600

    added react-router-dom dep

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import React from "react";
-import BrowserRouter from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Landing from "./pages/Landing";
+// import Contact from "./pages/Contact";
+// import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path={["/"]}>
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
