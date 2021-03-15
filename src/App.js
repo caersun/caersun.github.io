466bc6adaacfa6856b22a023048c7a7ea2acccde
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2021-03-15 02:36:25 -0500

    fixed typo in scheduler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Page from "./pages/Page";
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
